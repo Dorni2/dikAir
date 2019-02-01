b4db305776b286b390bb14e229fd044b2e3b9f2c
Dor <Dorni2@Gmail.com>
2019-02-01 10:57:30 +0200

    chat component created

--- dikAir/src/app/app.module.ts
+++ dikAir/src/app/app.module.ts
@@ -2,20 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 // import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Services
 import { ChatService } from './chat.service';
+
+// Components
+import { ChatComponent } from './chat/chat.component';
+
+const appRoutes:Routes = [
+  {path: 'chat', component: ChatComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [ChatService],
   bootstrap: [AppComponent]
